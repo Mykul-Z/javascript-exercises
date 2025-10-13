4fbf7924f8534103bdfd9eabefe1ec9f4b103961
Michael Zeng <zengmichael97@gmail.com>
2025-10-13 15:29:34 -0700

    Completed Foundations JS Exercise 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, repeats) 
+{
+    if (repeats < 0)
+    {
+        return "ERROR"
+    }
+    outputString = ""
+    for (i=0; i<repeats; i++)
+    {
+        outputString = outputString+inputString
+    }
+    
+    return outputString
 };
 
 // Do not edit below this line
