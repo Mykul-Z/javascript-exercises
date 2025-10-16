d0e25ef4dec3d3b8edd409bc90501e6a60a6e448
Michael Zeng <zengmichael97@gmail.com>
2025-10-15 20:45:27 -0700

    Completed Foundation JS Exercise 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,4 +1,13 @@
-const reverseString = function() {
+const reverseString = function(inputString) 
+{
+    outputArray = []
+
+    for (i = inputString.length-1; i >= 0; i--)
+    {
+        outputArray.push(inputString.charAt(i))
+    }
+
+    return outputArray.join("")
 
 };
 
