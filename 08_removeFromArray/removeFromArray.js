88c158daaf53b00de650b04e76b102c51c067eb8
Michael Zeng <zengmichael97@gmail.com>
2025-10-20 14:37:40 -0700

    Completed Foundations JS Exercise 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,31 @@
-const removeFromArray = function() {
+const removeFromArray = function() 
+{
+    argumentsArray = Array.from(arguments)
+    outputArray = []
+
+
+    if (argumentsArray.length == 1)
+    {
+        return argumentsArray[0]
+    }
+    else if (argumentsArray.length > 1)
+    {
+        inputArray = argumentsArray[0]
+        elements = argumentsArray.slice(1)
+        
+        for (i = 0; i< elements.length; i++)
+        {
+            if (inputArray.indexOf(elements[i] != -1))
+            {
+                    inputArray.splice([inputArray.indexOf(elements[i])], 1)
+            }      
+        }
+
+    }
+
+
+    return inputArray;
+
 };
 
 // Do not edit below this line
