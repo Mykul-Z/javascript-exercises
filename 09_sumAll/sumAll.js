3286e8ae9483e6bcd6c8f145735ffd9ea342539b
Michael Zeng <zengmichael97@gmail.com>
2025-10-20 14:46:37 -0700

    Completed Foundations JS Exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,34 @@
-const sumAll = function() {
+const sumAll = function(firstInput, secondInput) {
 
+    if (firstInput > secondInput)
+    {
+        start = secondInput;
+        finish = firstInput;
+    }
+    else
+    {
+        start = firstInput;
+        finish = secondInput;
+    }
+    if(Number.isInteger(start) != true || Number.isInteger(finish) != true)
+    {
+        return "ERROR"
+    }
+    else if(start < 0 || finish < 0)
+    {
+        return "ERROR"
+    }
+    else
+    {
+        sum = 0
+
+        for (i=start; start <= finish; start++)
+        {
+            sum = sum + start;
+        }
+
+        return sum;
+    }
 };
 
 // Do not edit below this line
